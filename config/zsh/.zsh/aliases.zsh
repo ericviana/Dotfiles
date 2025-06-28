117fa89da923da2923d553fc9c06c277c35a12f3
Eric Viana <ericvsdefault@gmail.com>
2025-06-28 16:45:14 -0300

    Initial commit

---
+++ config/zsh/.zsh/aliases.zsh
@@ -0,0 +1,33 @@
+# Config aliases
+alias zshconfig="nvim ~/.zshrc"
+alias vimconfig="vim ~/.config/nvim"
+alias zellijconfig="vim ~/.config/zellij"
+
+# Git aliases
+alias gcpb='git checkout -'
+alias gce='git commit --amend --no-edit --allow-empty'
+alias gs='git stash'
+alias gsp='git stash pop'
+alias gpo='git pull origin'
+
+# General aliases
+alias xcode="open -a Xcode"
+alias vim="nvim"
+alias c="clear"
+alias z="zellij"
+
+# Package managers
+alias pd="pnpm dev"
+alias pi="pnpm install"
+alias pb="pnpm build"
+alias nd="npm run dev"
+alias ni="npm install"
+alias nb="npm run build"
+
+# Rust
+alias cr="cargo run"
+
+# Scheduling shortcuts
+alias schedule-mr-daily='schedule "8:30" "mr" "daily"'
+alias schedule-mr-weekdays='schedule "8:30" "mr" "weekdays"'
+alias schedule-mr-monday='schedule "8:30" "mr" "monday"' 
